203bffabcd903c7351a662e7f6f7233800bc013b
andreshaitit <andreshaitit99@gmail.com>
2024-05-23 21:38:18 -0300

    fix images form

--- Frontend/src/components/Genre.js
+++ Frontend/src/components/Genre.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-function Genre(props){
+function Genre({categorie}){
     return(
         <React.Fragment>
             <div className="col-lg-6 mb-4">
                 <div className="card text-white bg-dark  shadow">
-                    <div className="card-body">
-                        {props.genre}
+                    <div className="card-body row">
+                        <div>
+                        {categorie?.name}
+                        </div>
+                        <div>
+                        {categorie?.name}
+                        </div>
                     </div>
                 </div>
             </div>
