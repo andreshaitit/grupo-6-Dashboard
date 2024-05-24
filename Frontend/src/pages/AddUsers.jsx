058a9511e6e8a21a3a6685f4371154caf2ce0e5f
andreshaitit <andreshaitit99@gmail.com>
2024-05-23 21:31:32 -0300

    edit

--- Frontend/src/pages/AddUsers.jsx
+++ Frontend/src/pages/AddUsers.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import FormUser from '../components/FormUser'
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { useLocation, useNavigate } from 'react-router';
 
 const AddUsers = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const data = location?.state?.data;
   return (
-    <div className='container-fluid p-5'>
-        <div className='row'>
-            <div className='col-12'><h1>Agregar Usuario</h1></div>
-            <div className='col-8 offset-1'>
-                <FormUser />
-            </div>
+    <div className="container-fluid p-5">
+      <div className="row">
+        <div className="col-2">
+          <IoIosArrowRoundBack
+            size={48}
+            onClick={() => navigate("/usuarios")}
+          />
         </div>
+        <div className="col-10">
+          <h1>{data ? "Editar Usuario" : "Agregar Usuario"}</h1>
+        </div>
+        <div className="col-8 offset-1">
+          <FormUser dataEdit={data} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default AddUsers
